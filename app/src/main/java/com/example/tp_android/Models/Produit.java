40ba3455cc47c847ea0e7dcc7d83a887db6bd0f6
1895127 <1895127@bdeb.qc.ca>
2019-08-12 11:13:37 -0400

    Ajout deux Methode Class Service Ajout Class Produit

--- app/src/main/java/com/example/tp_android/Models/Produit.java
+++ app/src/main/java/com/example/tp_android/Models/Produit.java
@@ -1,4 +1,64 @@
 package com.example.tp_android.Models;
 
 public class Produit {
+
+    String id;
+    String nom;
+    double prix;
+    int quantite;
+
+
+    public Produit() {
+    }
+
+
+    public Produit(String id, String nom, double prix, int quantite) {
+        this.id = id;
+        this.nom = nom;
+        this.prix = prix;
+        this.quantite = quantite;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    @Override
+    public String toString() {
+        return "Produit{" +
+                "id='" + id + '\'' +
+                ", nom='" + nom + '\'' +
+                ", prix=" + prix +
+                ", quantite=" + quantite +
+                '}';
+    }
 }
--- app/src/main/java/com/example/tp_android/Service/WebService.java
+++ app/src/main/java/com/example/tp_android/Service/WebService.java
@@ -1,4 +1,124 @@
 package com.example.tp_android.Service;
 
+import com.example.tp_android.Models.Produit;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
 public class WebService {
+
+    public List<Produit> getList() {
+
+        List<Produit> ListProduits = null;
+
+
+        URL url = null;
+
+        try {
+            url = new URL("http://ziedzaier.com/wp-content/uploads/2018/09/student2.txt");
+            HttpURLConnection client = null;
+
+            client = (HttpURLConnection) url.openConnection();
+
+            client.setRequestMethod("GET");
+
+            int responsecode = client.getResponseCode();
+
+            System.out.println("\n Sending 'GET' request to URL : " + url);
+            System.out.println("response Code : " + responsecode);
+
+
+            InputStreamReader myInput = new InputStreamReader(client.getInputStream());
+            BufferedReader in = new BufferedReader(myInput);
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            System.out.println(response.toString());
+
+            JSONArray produits = new JSONArray(response.toString());
+            JSONObject SingleProduit;
+            Produit p = null;
+            for (int i = 0; i < produits.length(); i++) {
+                SingleProduit = produits.getJSONObject(i);
+                p = new Produit();
+                p.setId(SingleProduit.getString("id"));
+                p.setNom(SingleProduit.getString("nom"));
+                p.setPrix(SingleProduit.getDouble("prix"));
+                p.setQuantite(SingleProduit.getInt("quantite"));
+
+            }
+            ListProduits.add(p);
+
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(ListProduits);
+        return ListProduits;
+
+    }
+
+
+    public Produit getProduit(String id) {
+        Produit p = new Produit();
+
+        URL url = null;
+
+        try {
+            url = new URL("http://ziedzaier.com/wp-content/uploads/2018/09/student2.txt");
+            HttpURLConnection client = null;
+
+            client = (HttpURLConnection) url.openConnection();
+
+            client.setRequestMethod("GET");
+
+            int responsecode = client.getResponseCode();
+
+            System.out.println("\n Sending 'GET' request to URL : " + url);
+            System.out.println("response Code : " + responsecode);
+
+
+            InputStreamReader myInput = new InputStreamReader(client.getInputStream());
+            BufferedReader in = new BufferedReader(myInput);
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            System.out.println(response.toString());
+
+            JSONObject JSONProduit = new JSONObject(response.toString());
+
+            p.setId(JSONProduit.getString("id"));
+            p.setNom(JSONProduit.getString("nom"));
+            p.setPrix(JSONProduit.getDouble("prix"));
+            p.setQuantite(JSONProduit.getInt("quantite"));
+
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(p);
+        return p;
+
+
+    }
+
+
 }
