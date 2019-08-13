2c434c0df705d48fa6e16ecf269bef80702ce6ce
Anis <aboutekadjiret@gmail.com>
2019-08-12 20:11:28 -0400

    Ajout deux methode dans WebService

--- app/src/main/java/com/example/tp_android/Android/MainActivity.java
+++ app/src/main/java/com/example/tp_android/Android/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.tp_android.Android;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tp_android.Models.Produit;
@@ -12,30 +15,54 @@ import com.example.tp_android.R;
 import com.example.tp_android.Service.WebService;
 import com.example.tp_android.Utilitaire.Verification;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
     EditText inUser,inPw;
     Intent myIntent;
+    ArrayList<Produit> list=new ArrayList();
+    Produit prd=new Produit();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+
+        WebService.GetListProduitsTask task= (WebService.GetListProduitsTask) new  WebService.GetListProduitsTask().execute();
+        WebService.GetProduitTask task2= (WebService.GetProduitTask) new WebService.GetProduitTask().execute("32");
+        try {
+            list=  task.get();
+            prd=task2.get();
+            System.out.println(list);
+            System.out.println(prd);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void login(View myView){
 
+        System.out.println("List egal= "+list);
+        System.out.println("Produit = "+prd);
         //instanciation de la zone de saisie
         inUser = (EditText) findViewById(R.id.editText);
         inPw = (EditText) findViewById(R.id.editText2);
-       // List<Produit> l = null;
-       // l= WebService.getList();
+
+
         String sUser = inUser.getText().toString();
         String sPw = inPw.getText().toString();
         System.out.println("sUser ---" +sUser);
         System.out.println("sPw ---" +sPw);
+
         if(Verification.validerUsername(sUser)&&Verification.validerPassword(sPw)){
             myIntent = new Intent(this,Main2Activity_succes.class);
             startActivity(myIntent);
--- app/src/main/java/com/example/tp_android/Service/WebService.java
+++ app/src/main/java/com/example/tp_android/Service/WebService.java
@@ -1,5 +1,10 @@
 package com.example.tp_android.Service;
 
+import android.os.AsyncTask;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import com.example.tp_android.Models.Produit;
 
 import org.json.JSONArray;
@@ -11,112 +16,115 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class WebService {
 
-    public static List<Produit> getList() {
 
-        List<Produit> ListProduits = null;
+    public static class GetListProduitsTask extends AsyncTask<Void, ArrayList<Produit>, ArrayList<Produit>> {
+
+        @Override
+        protected ArrayList<Produit> doInBackground(Void... params) {
+
+            ArrayList<Produit> ListProduits = new ArrayList();
+            URL url = null;
+
+            try {
+                url = new URL("http://192.168.2.20:8080/wsREST_simple/webresources/produit");
+                HttpURLConnection client = null;
+
+                client = (HttpURLConnection) url.openConnection();
+
+                client.setRequestMethod("GET");
+
+                int responsecode = client.getResponseCode();
+
+                System.out.println("\n Sending 'GET' request to URL : " + url);
+                System.out.println("response Code : " + responsecode);
 
 
-        URL url = null;
+                InputStreamReader myInput = new InputStreamReader(client.getInputStream());
+                BufferedReader in = new BufferedReader(myInput);
+                String inputLine;
+                StringBuffer response = new StringBuffer();
 
-        try {
-            url = new URL("http://localhost:52092/wsREST_simple/webresources/produit");
-            HttpURLConnection client = null;
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
 
-            client = (HttpURLConnection) url.openConnection();
+                JSONArray produits = new JSONArray(response.toString());
+                JSONObject SingleProduit;
+                Produit p = null;
+                for (int i = 0; i < produits.length(); i++) {
+                    SingleProduit = produits.getJSONObject(i);
+                    p = new Produit();
+                    p.setId(SingleProduit.getString("id"));
+                    p.setNom(SingleProduit.getString("nom"));
+                    p.setPrix(SingleProduit.getDouble("prix"));
+                    p.setQuantite(SingleProduit.getInt("quantite"));
+                    ListProduits.add(i, p);
+                }
 
-            client.setRequestMethod("GET");
 
-            int responsecode = client.getResponseCode();
-
-            System.out.println("\n Sending 'GET' request to URL : " + url);
-            System.out.println("response Code : " + responsecode);
-
-
-            InputStreamReader myInput = new InputStreamReader(client.getInputStream());
-            BufferedReader in = new BufferedReader(myInput);
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
             }
-            in.close();
+            return ListProduits;
 
-            System.out.println(response.toString());
-
-            JSONArray produits = new JSONArray(response.toString());
-            JSONObject SingleProduit;
-            Produit p = null;
-            for (int i = 0; i < produits.length(); i++) {
-                SingleProduit = produits.getJSONObject(i);
-                p = new Produit();
-                p.setId(SingleProduit.getString("id"));
-                p.setNom(SingleProduit.getString("nom"));
-                p.setPrix(SingleProduit.getDouble("prix"));
-                p.setQuantite(SingleProduit.getInt("quantite"));
-
-            }
-            ListProduits.add(p);
-
-        } catch (IOException | JSONException e) {
-            e.printStackTrace();
         }
-
-        System.out.println(ListProduits);
-        return ListProduits;
 
     }
 
 
-    public Produit getProduit(String id) {
-        Produit p = new Produit();
+    public static class GetProduitTask extends AsyncTask<String, Produit, Produit> {
 
-        URL url = null;
+        @Override
+        protected Produit doInBackground(String... params) {
+            String id = params[0];
+            Produit p = new Produit();
+            URL url = null;
 
-        try {
-            url = new URL("http://localhost:52092/wsREST_simple/webresources/produit/id/"+id);
-            HttpURLConnection client = null;
+            try {
 
-            client = (HttpURLConnection) url.openConnection();
+                url = new URL("http://192.168.2.20:8080/wsREST_simple/webresources/produit/id/" + id);
+                HttpURLConnection client = null;
 
-            client.setRequestMethod("GET");
+                client = (HttpURLConnection) url.openConnection();
 
-            int responsecode = client.getResponseCode();
+                client.setRequestMethod("GET");
 
-            System.out.println("\n Sending 'GET' request to URL : " + url);
-            System.out.println("response Code : " + responsecode);
+                int responsecode = client.getResponseCode();
+
+                System.out.println("\n Sending 'GET' request to URL : " + url);
+                System.out.println("response Code : " + responsecode);
 
 
-            InputStreamReader myInput = new InputStreamReader(client.getInputStream());
-            BufferedReader in = new BufferedReader(myInput);
-            String inputLine;
-            StringBuffer response = new StringBuffer();
+                InputStreamReader myInput = new InputStreamReader(client.getInputStream());
+                BufferedReader in = new BufferedReader(myInput);
+                String inputLine;
+                StringBuffer response = new StringBuffer();
 
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                JSONObject JSONProduit = new JSONObject(response.toString());
+
+                p.setId(JSONProduit.getString("id"));
+                p.setNom(JSONProduit.getString("nom"));
+                p.setPrix(JSONProduit.getDouble("prix"));
+                p.setQuantite(JSONProduit.getInt("quantite"));
+
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
             }
-            in.close();
+            return p;
 
-            System.out.println(response.toString());
-
-            JSONObject JSONProduit = new JSONObject(response.toString());
-
-            p.setId(JSONProduit.getString("id"));
-            p.setNom(JSONProduit.getString("nom"));
-            p.setPrix(JSONProduit.getDouble("prix"));
-            p.setQuantite(JSONProduit.getInt("quantite"));
-
-        } catch (IOException | JSONException e) {
-            e.printStackTrace();
         }
-
-        System.out.println(p);
-        return p;
-
 
     }
 
