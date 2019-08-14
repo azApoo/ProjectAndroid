72f58a9c6430e97024bc534dff517adf3956affc
mihadadene <mi.ihadadene@gmail.com>
2019-08-14 09:58:36 -0400

    correction mineur

--- app/src/main/java/com/example/tp_android/Android/Main3Activity.java
+++ app/src/main/java/com/example/tp_android/Android/Main3Activity.java
@@ -40,7 +40,7 @@ public class Main3Activity extends AppCompatActivity {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        out1.setText("PRODUITS DETAILS");
+        out1.setText("PRODUIT DETAILS");
         out2.setText(prd.getId());
         out3.setText(prd.getNom());
         out4.setText(""+ prd.getPrix());
